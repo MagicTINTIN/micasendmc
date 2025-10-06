97fd517ecd5cc45e8fd7bc0918c308d186007386
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-10-06 12:21:52 +0200

    initial commit : template

---
+++ src/main/java/fr/magictintin/micasendmc/MicaSendDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.magictintin.micasendmc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MicaSendDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
