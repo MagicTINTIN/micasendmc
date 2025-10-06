ad0f47c58334136f82a36f768b54670f0bca767b
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-10-06 16:04:09 +0200

    feat: sending messages to micasend instance

--- src/main/java/fr/magictintin/micasendmc/MicaSend.java
+++ src/main/java/fr/magictintin/micasendmc/MicaSend.java
@@ -15,10 +15,10 @@ public class MicaSend implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	protected static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	private final static String MICASEND_URL = "https://micasend.magictintin.fr";
-	private static final String WS_URL = "wss://msws.magictintin.fr:8443";
+	protected final static String MICASEND_URL = "https://micasend.magictintin.fr";
+	protected static final String WS_URL = "wss://msws.magictintin.fr:8443";
 
 	@Override
 	public void onInitialize() {
