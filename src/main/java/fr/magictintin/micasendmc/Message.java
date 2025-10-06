ec2233b10f4c1dd7288684f7e4bcfbbb83c4e284
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-10-06 14:12:53 +0200

    feat: sending message once client started

---
+++ src/main/java/fr/magictintin/micasendmc/Message.java
@@ -0,0 +1,19 @@
+package fr.magictintin.micasendmc;
+
+public class Message {
+    public String id, content, sender, date_time, id_certified_user, rank;
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id='" + id + "'}";
+    }
+
+    public int id() {
+        try {
+            return Integer.parseInt(id);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+}
--- src/main/java/fr/magictintin/micasendmc/MicaSend.java
+++ src/main/java/fr/magictintin/micasendmc/MicaSend.java
@@ -2,6 +2,8 @@ package fr.magictintin.micasendmc;
 
 import net.fabricmc.api.ModInitializer;
 
+import javax.swing.text.html.parser.Entity;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +15,26 @@ public class MicaSend implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private final static String MICASEND_URL = "https://micasend.magictintin.fr";
+    private static final String WS_URL = "wss://msws.magictintin.fr:8443";
+    
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Micasend started successfully!");
+		Connector.sendMessage(MICASEND_URL, "Someone", "Started minecraft", () -> {
+            // ws.sendMessage("new micasend message");
+            // fetchMessages();
+			System.out.println("Sent information minecraft has been started...");
+        });
 	}
+
+    @FunctionalInterface
+    public static interface VoidCallback {
+        public void execute();
+    }
 }
