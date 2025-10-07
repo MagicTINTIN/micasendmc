b6d548f6a672fd44e1541f15aa7ddd8f4a789fad
MagicTINTIN <49071368+MagicTINTIN@users.noreply.github.com>
2025-10-07 16:55:57 +0200

    feat: websocket connection to get updated when there is a new message

--- src/main/java/fr/magictintin/micasendmc/MicaSend.java
+++ src/main/java/fr/magictintin/micasendmc/MicaSend.java
@@ -1,11 +1,14 @@
 package fr.magictintin.micasendmc;
 
+import java.util.ArrayList;
+
+// import org.apache.commons.lang3.StringEscapeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+// import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+// import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
 
@@ -19,6 +22,8 @@ public class MicaSend implements ModInitializer {
 
 	protected final static String MICASEND_URL = "https://micasend.magictintin.fr";
 	protected static final String WS_URL = "wss://msws.magictintin.fr:8443";
+	private WebsocketClient ws;
+	protected static int maxID = 0;
 
 	@Override
 	public void onInitialize() {
@@ -46,8 +51,18 @@ public class MicaSend implements ModInitializer {
 			System.out.println("Sent information minecraft has been started...");
 		});
 
+		this.ws = new WebsocketClient(WS_URL, () -> fetchMessages());
 	}
-
+	
+	private synchronized void fetchMessages() {
+        ArrayList<Message> list = Connector.fetchMessages(MICASEND_URL);
+        for (Message msg : list) {
+            if (msg.id() <= maxID)
+                continue;
+            System.out.println("MESSAGE: [" + msg.sender + "] " + msg.content);
+            // System.out.println(" MICASEND (" + i + ") >>> [" + msg.sender + "]: " + msg.content.replace("§", " "));
+        }
+    }
 	@FunctionalInterface
 	public static interface VoidCallback {
 		public void execute();
